2fc6b394f11bea7cc1eb0ff189351adb5558e5f1
Sumanth Yedoti <sumanth.yedoti@gmail.com>
2022-04-12 22:36:48 +0530

    Implement word-counting problem

--- ch-1/ex-1.10/main.c
+++ ch-1/ex-1.10/main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 
+/** Write a program to copy its input to its output, replacing each tab by \t ,
+ * each backspace by \b , and each backslash by \\ */
+
 int main()
 {
   int c;
